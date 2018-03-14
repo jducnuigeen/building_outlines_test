bac04e8f7a1a957d48f961e9a361af12717daac8
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-03-14 16:43:11 +1300

    c

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -286,7 +286,7 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-schema = json.load(file('data/linz_db_schema2.json'))
+schema = json.load(open('data/linz_db_schema2.json'))
 
 html_context = {
 	'outputschema': schema
