810f51ab17fe5e8dc242a67ef4680360adad726d
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-03-19 13:49:47 +1300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 import json
-import tabulate
+from tabulate import tabulate
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -290,10 +290,18 @@ texinfo_documents = [
 
 # Extensions
 
-#building_name_table = [
-#                ["building_name_id", character, 250, noprec, noscl, "An Id for a building name: 5","A hardware store" ],
-#                ["building_id", integer, 4, noprec, noscl, "3928", "A unique id for a building" ]
-#                ]
+# Test Data
+building_name_table = [
+                ["building_name_id", "character", 250, "noprec", "noscl", "An Id for a building name: 5","A hardware store" ],
+                ["building_id", "integer", 4, "noprec", "noscl", "3928", "A unique id for a building" ]
+                ]
+rst_table = tabulate(building_name_table,tablefmt='rst')
+
+#json.dumps(rst_table)
+
+with open('dbschema.json', 'w') as outfile:
+    json.dump(rst_table, outfile)
+
 
 
 def rstjinja(app, docname, source):
---
+++ docs/source/create_tables.py
@@ -0,0 +1,9 @@
+from tabulate import tabulate
+
+building_name_table = [
+                ["building_name_id", "character", 250, "noprec", "noscl", "An Id for a building name: 5","A hardware store" ],
+                ["building_id", "integer", 4, "noprec", "noscl", "3928", "A unique id for a building" ]
+                ]
+
+
+rst_table = tabulate(building_name_table,tablefmt='rst')
