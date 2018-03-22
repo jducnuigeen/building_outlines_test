2fdb944653a0176fffec4986658f59ac50f083ec
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-03-22 15:39:46 +1300

    asdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ from os import path
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -310,7 +310,7 @@ def get_schema_name():
     # the path of the sql file is:
     # home/jducnuigeen/dev/building_outlines_test/sql/
     #sql_file_path = path.abspath(path.join(script_file_path, "..", "..", "./sql/building_outlines_test/sql/01-create_buildings_schema.sql"))
-    sql_file_path = path.abspath(path.join(script_file_path,"/sql/building_outlines_test/sql/01-create_buildings_schema.sql"))
+    sql_file_path = path.abspath(path.join(script_file_path,"/building_outlines_test/sql/01-create_buildings_schema.sql"))
     with open(sql_file_path) as f:
         for line in f:
             schemaname = re.search(r"(?:CREATE SCHEMA IF NOT EXISTS)\s(.*)(;)", line)
