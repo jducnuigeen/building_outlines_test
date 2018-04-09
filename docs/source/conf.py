57b7f3f2c051b925ed43f8ff4f1ca3a69a44a47f
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-04-09 12:01:59 +1200

    asdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -169,7 +169,7 @@ html_static_path = ['_static']
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
 html_additional_pages = {
-    'schema1': 'schema1.html'
+    'All Schema': 'schema1.html'
 
 
 }
