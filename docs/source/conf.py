a3dbf2cc122713aed31ffebd122ff09f22ece3f1
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-04-10 12:23:53 +1200

    oj

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -153,7 +153,7 @@ html_static_path = ['_static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ['../sql']
+html_extra_path = ['..sql']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
