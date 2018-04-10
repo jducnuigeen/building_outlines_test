979c06ff9a5c86ddedf6e1c0567364e86e314ce0
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-04-10 12:01:47 +1200

    asd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -306,7 +306,8 @@ texinfo_documents = [
 # ../docs/source
 # The path of the SQL file has been altered from likely final folder path
 
-sql_file_path = "./sql_not_final_location/02-buildings_schema.sql"
+sql_file_path = "sql/02-buildings_schema.sql"
+#sql_file_path = "./sql_not_final_location/02-buildings_schema.sql"
 #sql_file_path = "./sql_not_final_location/02-create_buildings_stage_schema.sql"
 
 def get_schema():
