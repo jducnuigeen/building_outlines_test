be9608477a6e680dad1156bf4c4af4195d31f16f
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-04-17 16:05:26 +1200

    sdf

---
+++ sql/lds/01-buildings_lds_schema.sql
@@ -0,0 +1,28 @@
+------------------------------------------------------------------------------
+-- Create buildings LDS schema and tables
+------------------------------------------------------------------------------
+
+SET client_min_messages TO WARNING;
+
+
+CREATE SCHEMA IF NOT EXISTS buildings_lds;
+
+-- NZ Building Outlines
+
+CREATE TABLE IF NOT EXISTS buildings_lds.nz_building_outlines (
+      building_outline_id integer NOT NULL
+    , building_id integer NOT NULL
+    , name character varying(250) NOT NULL
+    , use character varying(40) NOT NULL
+    , suburb_locality character varying(80) NOT NULL
+    , town_city character varying(80) NOT NULL
+    , territorial_authority character varying(80) NOT NULL
+    , capture_method character varying(250) NOT NULL
+    , capture_source character varying(250) NOT NULL
+    , lifecycle_stage character varying(250) NOT NULL
+    , outline_begin_lifespan timestamptz NOT NULL
+    , building_begin_lifespan timestamptz NOT NULL
+    , name_begin_lifespan timestamptz NOT NULL
+    , use_begin_lifespan timestamptz NOT NULL
+    , shape public.geometry(MultiPolygon, 2193) NOT NULL
+);
