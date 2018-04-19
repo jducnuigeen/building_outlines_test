b70b8fff7cfeb8f2a6e5f1a6eed73e5edac762ac
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-04-19 16:42:35 +1200

    sdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -168,7 +168,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
+html_sidebars = {'**': ['about.html'] }
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
