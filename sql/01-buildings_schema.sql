5c2a7116d92a21ecbf55c9e03aae0fa5444d8482
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-04-27 10:12:28 +1200

    asd

--- sql/01-buildings_schema.sql
+++ sql/01-buildings_schema.sql
@@ -22,7 +22,6 @@ COMMENT ON TABLE buildings.lifecycle_stage IS
 'Lookup table that holds all of the lifecycle stages for a building.';
 
 COMMENT ON COLUMN buildings.lifecycle_stage.lifecycle_stage_id IS
-# ':ref:`table-name-use`';
 'Does anything work here';
 COMMENT ON COLUMN buildings.lifecycle_stage.value IS
 'The stage of a buildings lifecycle..';
