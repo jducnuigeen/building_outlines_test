a58d191ecfbf9bddbb1717f316c38bf415416f6b
Jan Ducnuigeen <jducnuigeen@linz.govt.nz>
2018-04-30 14:27:35 +1200

    sdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -455,7 +455,7 @@ def get_columns(table_str, file_content, this_table_columns):
             pri_key = pri_key_serial_search.group(1)
             pri_key2 = pri_key.strip()
             column_str = table_str + "." + pri_key2
-            this_column.append(" **" + pri_key + "** ")  #column Name
+            this_column.append(" `**" + pri_key + "**` ")  #column Name
             this_column.append("integer")  #Data Type
             this_column.append(" ")  # Length
             this_column.append("32")  #Precision
